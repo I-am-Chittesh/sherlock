7891de21bba44d9cd59378641c60c8abcb79cf8a
I-am-Chittesh <iamchitteshd@gmail.com>
2026-08-31 14:07:34 +0530

    main

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+
+// Vue Flow base styles
 import '@vue-flow/core/dist/style.css'
 import '@vue-flow/core/dist/theme-default.css'
 
