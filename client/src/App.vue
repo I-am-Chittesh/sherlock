a326f66f96067ab187e43adaa37881127d539d18
I-am-Chittesh <iamchitteshd@gmail.com>
2026-08-31 13:25:57 +0530

    updatE

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,40 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { store } from './store';
+import ConsoleView from './components/ConsoleView.vue';
+import HubView from './components/HubView.vue';
+import PinboardView from './components/PinboardView.vue';
+import AdminView from './components/AdminView.vue';
+import Taskbar from './components/Taskbar.vue';
+
+const handlePaginateLeft = () => {
+  window.dispatchEvent(new CustomEvent('taskbar-left'));
+};
+
+const handlePaginateRight = () => {
+  window.dispatchEvent(new CustomEvent('taskbar-right'));
+};
+
+const handleOpenModal = () => {
+  window.dispatchEvent(new CustomEvent('taskbar-plus'));
+};
 </script>
 
 <template>
-  <HelloWorld />
+  <main class="w-screen h-screen overflow-hidden bg-[#0a0a0a] text-white relative">
+    
+    <!-- Screen Router -->
+    <ConsoleView v-if="store.currentScreen === 'console'" />
+    <HubView v-if="store.currentScreen === 'hub'" />
+    <PinboardView v-if="store.currentScreen === 'pinboard'" />
+    <AdminView v-if="store.currentScreen === 'admin'" />
+
+    <!-- Persistent Floating Pill Taskbar -->
+    <Taskbar 
+      v-if="store.currentScreen !== 'console'" 
+      @paginate-left="handlePaginateLeft"
+      @paginate-right="handlePaginateRight"
+      @open-modal="handleOpenModal"
+    />
+
+  </main>
 </template>
